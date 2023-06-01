2b45d9bb70330996cf8539161e6ca89a19903e73
DESKTOP-FN4R272\WPOSS <cruzdiaz0819@gmail.com>
2023-06-01 16:10:51 -0500

    Action_bar
    Termine de incluir los action_bar del botoom, todos en todas las actividades son funcionales, el cambio de actividades se ve raro, tratar de mejorar eso.
    Faltan muchas cosas, subir posts y ver posts debo implementarlo, ya se como hacerlo.
    Falta agregar el modelo mvp.

--- app/src/main/java/com/example/apionlyfans/perfil_ajustes.java
+++ app/src/main/java/com/example/apionlyfans/perfil_ajustes.java
@@ -2,13 +2,68 @@ package com.example.apionlyfans;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class perfil_ajustes extends AppCompatActivity {
+
+    ImageButton home;
+    ImageButton explorar;
+    ImageButton posts;
+    ImageButton perfil;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_ajustes);
+
+        home = findViewById(R.id.home);
+        explorar = findViewById(R.id.explorar);
+        posts = findViewById(R.id.posts);
+        perfil = findViewById(R.id.perfil);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(perfil_ajustes.this, menu_inicial.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        explorar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(perfil_ajustes.this, sugerencias_perfiles.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        posts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(perfil_ajustes.this, subir_posts.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        perfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(perfil_ajustes.this, perfil_ajustes.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/apionlyfans/subir_posts.java
+++ app/src/main/java/com/example/apionlyfans/subir_posts.java
@@ -2,13 +2,68 @@ package com.example.apionlyfans;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class subir_posts extends AppCompatActivity {
+
+    ImageButton home;
+    ImageButton explorar;
+    ImageButton posts;
+    ImageButton perfil;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subir_posts);
+
+        home = findViewById(R.id.home);
+        explorar = findViewById(R.id.explorar);
+        posts = findViewById(R.id.posts);
+        perfil = findViewById(R.id.perfil);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(subir_posts.this, menu_inicial.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        explorar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(subir_posts.this, sugerencias_perfiles.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        posts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(subir_posts.this, subir_posts.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        perfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(subir_posts.this, perfil_ajustes.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
